628deeca9ecf7c3446eb80bee75462a34ae0ac6a
Omar Siam <Omar.Siam@oeaw.ac.at>
2023-10-06 16:11:24 +0200

    Add a change operation for the cached documents

--- dboeannotation/services/async_api_service.py
+++ dboeannotation/services/async_api_service.py
@@ -535,6 +535,32 @@ async def api_documents_list(
             return response
 
 
+async def api_documents_change(data: List[Dict[str, Any]], api_config_override: Optional[APIConfig] = None) -> None:
+    api_config = api_config_override if api_config_override else APIConfig()
+
+    base_path = api_config.base_path
+    path = f"/api/documents/"
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer { api_config.get_access_token() }",
+    }
+
+    query_params: Dict[str, Any] = {}
+
+    query_params = {key: value for (key, value) in query_params.items() if value is not None}
+
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.request(
+            "put", base_path + path, params=query_params, json=[i.dict() for i in data]
+        ) as inital_response:
+            if inital_response.status != 200:
+                raise HTTPException(inital_response.status, f" failed with status code: {inital_response.status}")
+            response = await inital_response.json()
+
+            return None
+
+
 async def api_documents_read(
     id: int,
     es_id: Optional[str] = None,
