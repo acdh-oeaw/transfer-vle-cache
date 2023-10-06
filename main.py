086f4dfcd1f1453134b09d731e184dde376b4c81
Omar Siam <Omar.Siam@oeaw.ac.at>
2023-10-06 17:15:51 +0200

    Update the cache entries
    On successful write to the XML database remove the cached XML.
    On error put the error message to the dboeannotation server.

--- main.py
+++ main.py
@@ -5,23 +5,25 @@ import nest_asyncio
 nest_asyncio.apply()
 
 from dboeannotation.services.async_api_token_auth_service import api_token_auth_create
-from dboeannotation.services.async_api_service import api_documents_list
-from vleserver.services.async_entries_service import _getDictDictNameEntries
-from vleserver.services.async_entries_service import _changeEntries
+from dboeannotation.services.async_api_service import api_documents_list, api_documents_change
+from dboeannotation.api_config import APIConfig as dboeannotationAPIConfig
+from vleserver.services.async_entries_service import _getDictDictNameEntries, _changeEntries
 from vleserver.api_config import HTTPException
 
+access_config = dboeannotationAPIConfig()
+
 async def getXmlFromCache() -> Dict:
-    from dboeannotation.api_config import APIConfig;
-    config = APIConfig()
-    response = await api_token_auth_create({'username': 'vle', 'password': 'changeme_please'}, config)
+    response = await api_token_auth_create({'username': 'vle', 'password': 'changeme_please'}, access_config)
     # print(response['token'])
-    config.set_access_token(response['token'])
-    return await api_documents_list(cache_only=True, page_size=50, api_config_override=config)
+    access_config.set_access_token(response['token'])
+    return await api_documents_list(cache_only=True, page_size=50, api_config_override=access_config)
 
 # There is a limit on how many ids can be passed in one query.
 max_num_of_ids = 50
 
-async def sendUpdate(cachedXML: Dict, es_ids: List, by: str) -> List:
+async def sendUpdate(cachedData: Dict, es_ids: List, by: str) -> List:
+    cachedXML = dict((result['es_id'], result['xml']) for result in cachedData['results'])
+    # print(cachedXML)
     try:
       vleData = await _getDictDictNameEntries(dict_name='_qdb-TEI-02', pageSize=max_num_of_ids, ids=','.join(es_ids), lock=5)
     except HTTPException as error:
@@ -36,14 +38,23 @@ async def sendUpdate(cachedXML: Dict, es_ids: List, by: str) -> List:
     #for id in cachedXML.keys():
     #    print((id, [e for e in vleData['_embedded']['entries'] if e['id'] == id]))
     try:
-        return [await _changeEntries(dict_name='_qdb-TEI-02', as_user=by, data=vleData['_embedded'])]
+        res = await _changeEntries(dict_name='_qdb-TEI-02', as_user=by, data=vleData['_embedded'])
+        ids = [entry['id'] for entry in res['_embedded']['entries']]
+        changeData = [result for result in cachedData['results'] if result['es_id'] in ids]
+        for change in changeData:
+            change['xml'] = ''
+        await api_documents_change(changeData, api_config_override=access_config)
+        return [res]
     except HTTPException as error:
         if len(es_ids) == 1:
+            changeData = [result for result in cachedData['results'] if result['es_id'] == es_ids[0]]
+            changeData[0]['xml_error_message'] = error.details['detail']
+            await api_documents_change(changeData, api_config_override=access_config)
             return [error.details]
         else:
             res = []
             for es_id in es_ids:
-                res.append(*(await sendUpdate(cachedXML, [es_id], by)))
+                res.append(*(await sendUpdate(cachedData, [es_id], by)))
             return res
 
 async def main():
@@ -52,9 +63,7 @@ async def main():
     # print(cachedData)
     for by in {result["xml_modified_by"] for result in cachedData["results"]}:
         es_ids = [result['es_id'] for result in cachedData['results'] if result["xml_modified_by"] == by][:max_num_of_ids]
-        cachedXML = dict((result['es_id'], result['xml']) for result in cachedData['results'])
-        # print(cachedXML)
-        print({by: await sendUpdate(cachedXML, es_ids, by)})
+        print({by: await sendUpdate(cachedData, es_ids, by)})
 
 if __name__ == '__main__':
     asyncio.run(main())
