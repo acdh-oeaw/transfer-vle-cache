76d1313e510278f5bc5184fd4cf2ab81d13adbc3
Omar Siam <Omar.Siam@oeaw.ac.at>
2023-10-06 16:00:12 +0200

    React on errors, try with single id, report error on the console

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 import asyncio
 import nest_asyncio
 
@@ -8,6 +8,7 @@ from dboeannotation.services.async_api_token_auth_service import api_token_auth_
 from dboeannotation.services.async_api_service import api_documents_list
 from vleserver.services.async_entries_service import _getDictDictNameEntries
 from vleserver.services.async_entries_service import _changeEntries
+from vleserver.api_config import HTTPException
 
 async def getXmlFromCache() -> Dict:
     from dboeannotation.api_config import APIConfig;
@@ -17,24 +18,43 @@ async def getXmlFromCache() -> Dict:
     config.set_access_token(response['token'])
     return await api_documents_list(cache_only=True, page_size=50, api_config_override=config)
 
+# There is a limit on how many ids can be passed in one query.
+max_num_of_ids = 50
 
-async def main():
-    from vleserver.api_config import APIConfig;
-    cachedData = await getXmlFromCache()
-    # print(cachedData)
-    es_ids = ','.join([result['es_id'] for result in cachedData['results']])
-    cachedXML = dict((result['es_id'], result['xml']) for result in cachedData['results'])
-    # print(cachedXML)
-    vleData = await _getDictDictNameEntries(dict_name='_qdb-TEI-02', pageSize=50, ids=es_ids, lock=5)
+async def sendUpdate(cachedXML: Dict, es_ids: List, by: str) -> List:
+    try:
+      vleData = await _getDictDictNameEntries(dict_name='_qdb-TEI-02', pageSize=max_num_of_ids, ids=','.join(es_ids), lock=5)
+    except HTTPException as error:
+        # print(error.message, error.details)
+        return [error.details]
     # print(vleData['_embedded']['entries'])
     for id in cachedXML.keys():
         l = [entry for entry in vleData['_embedded']['entries'] if entry['id'] == id]
         entry = l[0] if len(l) == 1 else None
         if entry is not None:
             entry['entry'] = cachedXML[id]
-    # for id in cachedXML.keys():
-    #     print((id, [e for e in vleData['_embedded']['entries'] if e['id'] == id]))
-    await _changeEntries(dict_name='_qdb-TEI-02', as_user='another-user', data=vleData['_embedded'])
+    #for id in cachedXML.keys():
+    #    print((id, [e for e in vleData['_embedded']['entries'] if e['id'] == id]))
+    try:
+        return [await _changeEntries(dict_name='_qdb-TEI-02', as_user=by, data=vleData['_embedded'])]
+    except HTTPException as error:
+        if len(es_ids) == 1:
+            return [error.details]
+        else:
+            res = []
+            for es_id in es_ids:
+                res.append(*(await sendUpdate(cachedXML, [es_id], by)))
+            return res
+
+async def main():
+    from vleserver.api_config import APIConfig;
+    cachedData = await getXmlFromCache()
+    # print(cachedData)
+    for by in {result["xml_modified_by"] for result in cachedData["results"]}:
+        es_ids = [result['es_id'] for result in cachedData['results'] if result["xml_modified_by"] == by][:max_num_of_ids]
+        cachedXML = dict((result['es_id'], result['xml']) for result in cachedData['results'])
+        # print(cachedXML)
+        print({by: await sendUpdate(cachedXML, es_ids, by)})
 
 if __name__ == '__main__':
     asyncio.run(main())
